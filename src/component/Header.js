7e6fc38532550ba56ef2e2073cf8160bffbbdb2f
Didier <dyder1493@gmail.com>
2021-04-07 16:18:38 +0200

    finish link router

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
-import { Link }  from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     title: {
-        flexGrow: 1
+        flexGrow: 5
     }
 }));
 
 const Header = () => {
     const classes = useStyles();
+    const history = useHistory();
     return (
         <div >
             <AppBar position="static">
                 <Toolbar>
-                    <Typography className={classes.title}/>
-                    <Link to="/">Home</Link>
-                    <Link to="/items">Items</Link>
-                    <Link to="/admin">Admin</Link>
-                    <Link to="/liens">Liens</Link>
+                    <Typography className={classes.title} />
+                    <Button color="inherit" onClick={() => history.push('/')}>Home</Button>
+                    <Button color="inherit" onClick={() => history.push('/items')}>Items</Button>
+                    <Button color="inherit" onClick={() => history.push('/admin')}>Admin</Button>
+                    <Button color="inherit" onClick={() => history.push('/liens')}>Liens</Button>
                 </Toolbar>
             </AppBar>
         </div>
